37736edb10e39f643ce3310861671cb98787ebf6
MarcoConcilio <marcoconcilio22@gmail.com>
2026-07-02 20:23:45 +0200

    feat: add issue labels utility

---
+++ frontend/src/utils/issueLabels.js
@@ -0,0 +1,9 @@
+export const issueTypeLabels = {
+  QUESTION: 'Domanda', BUG: 'Bug', DOCUMENTATION: 'Documentazione', FEATURE: 'Funzionalità'
+};
+export const priorityLabels = {
+  LOW: 'Bassa', MEDIUM: 'Media', HIGH: 'Alta', CRITICAL: 'Critica'
+};
+export const statusLabels = {
+  TODO: 'Da fare', IN_PROGRESS: 'In corso', DONE: 'Completata'
+};
